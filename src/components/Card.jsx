30b4483b44deb61b7f9a4c3c1d1f7d635f274148
Paul Caoile <hyp3rsp8c3@gmail.com>
2023-09-10 18:38:13 -0400

    update css styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 export default class Card extends Component {
   constructor(props) {
     super(props);
-
+    let angle = Math.random() * 90 - 45;
+    this._transform = `rotate(${angle}deg)`;
   }
+
   render() {
     return (
-      <img src={ this.props.imgSrc } alt={ this.props.cardName } className='p-4' />
+      <img src={ this.props.imgSrc } alt={ this.props.cardName } className='p-1' style={ { transform: this._transform } } />
     );
   }
 }
